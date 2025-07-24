7a5cea23beaf5aa5e316902162d437aabd376efd
Farhan i <b21ds062@mace.ac.in>
2025-07-25 02:12:49 +0530

    patient-detailes fixed

--- src/app/patients/patient-details/patient-details.component.ts
+++ src/app/patients/patient-details/patient-details.component.ts
@@ -1,6 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Patient } from '../../core/models/patient.model';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { PatientService } from '../services/patient.service';
 import { Location } from '@angular/common';
 
 @Component({
@@ -12,11 +14,33 @@ import { Location } from '@angular/common';
   templateUrl: './patient-details.component.html',
   styleUrls: ['./patient-details.component.scss']
 })
-export class PatientDetailsComponent {
-  @Input() patient: Patient | undefined;
-  loading = false;
+export class PatientDetailsComponent implements OnInit {
+  patient: Patient | undefined;
+  loading = true;
 
-  constructor(private location: Location) {}
+  constructor(
+    private route: ActivatedRoute,
+    private patientService: PatientService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    if (id) {
+      this.patientService.getPatientById(id).subscribe({
+        next: (pat) => {
+          this.patient = pat;
+          this.loading = false;
+        },
+        error: (err) => {
+          this.patient = undefined;
+          this.loading = false;
+        }
+      });
+    } else {
+      this.loading = false;
+    }
+  }
 
   goBack(): void {
     this.location.back();
